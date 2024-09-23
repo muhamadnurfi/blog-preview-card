6fd904fd27a476e690bcadd26d30fda0126b8038
muhamadnurfi <muhammadnurfi11@gmail.com>
2024-09-23 15:25:19 +0700

    feat: desktop

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#F4D04E",
+        grey: "#6B6B6B",
+      },
+      fontSize: {
+        small: "14px",
+        heading: "24px",
+      }
+    },
+  },
+  plugins: [],
+};
+
